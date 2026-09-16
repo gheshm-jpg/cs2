43904f945ac9876b5e1f54d3c904464ca9ca350a
gheshm-jpg <gheshm@bc.edu>
2026-09-16 15:30:01 -0400

    added comments for readability

--- TaxTriple.cpp
+++ TaxTriple.cpp
@@ -6,7 +6,9 @@ using std::cin;
 
 double sales_tax;
 int total;
+
 //let t be each pretax price
+
 int t1;
 int t2;
 int t3;
@@ -14,8 +16,11 @@ int t3;
 
 int main(){
 	sales_tax=1.0625;
-	cin >> t1 >> t2 >> t3;		
+	//input each t
+	cin >> t1 >> t2 >> t3;
+	//applies sales_tax to p, rounds to a whole number, then outputs each result		
 	cout << round(t1*sales_tax) << ' ' << round(t2*sales_tax) << ' '<< round(t3*sales_tax) << endl;
+	//calculates the total after sales_tax and rounding
 	total= round(t1*sales_tax) + round(t2*sales_tax) + round(t3*sales_tax);
 	cout << "Total: " << total << endl;	
 return 0;
