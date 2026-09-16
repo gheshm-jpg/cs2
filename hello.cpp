49e3409c7aed15dce465909048b05aae4a4db857
gheshm-jpg <gheshm@bc.edu>
2026-09-16 13:05:08 -0400

    fixed missing double quote and unneccessary + symbol

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // Create a variable holding 100 and output it
     int harmonica = 100;
-    cout << "harmonica: << harmonica+ << endl;
+    cout << "harmonica:" << harmonica << endl;
     
     // Compute the fifth harmonic number
     double harmonic = 1/1 + 1/2 + 1/3 + 1/4 + 1/5;
