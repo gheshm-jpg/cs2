ad7dcbe19610af513c70b22cedc68786e69db468
gheshm-jpg <gheshm@bc.edu>
2026-09-16 14:03:35 -0400

    fixed harmonic being unused, used floating-point operands, such as 1.0/1 + 1.0/2 + 1.0/3 + 1.0/4 + 1.0/5, so division preserves the fractional parts

--- hello.cpp
+++ hello.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << "harmonica:" << harmonica << endl;
     
     // Compute the fifth harmonic number
-    double harmonic = 1/1 + 1/2 + 1/3 + 1/4 + 1/5;
-    cout << "H_5 = " << harmonica << endl;
+    double harmonic = 1/1 + 1/2.0 + 1/3.0 + 1/4.0 + 1/5.0;
+    cout << "H_5 = " << harmonic << endl;
     
     return 0;
 }
