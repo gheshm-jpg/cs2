1ec1cd3303406ff715e7e4b5edb8eff34e0bebbe
gheshm-jpg <gheshm@bc.edu>
2026-09-16 15:39:30 -0400

    almost lost 1 billion points

--- TaxTriple.cpp
+++ TaxTriple.cpp
@@ -18,7 +18,7 @@ int main(){
 	sales_tax=1.0625;
 	//input each t
 	cin >> t1 >> t2 >> t3;
-	//applies sales_tax to p, rounds to a whole number, then outputs each result		
+	//applies sales_tax to each t, rounds to a whole number, then outputs each result		
 	cout << round(t1*sales_tax) << ' ' << round(t2*sales_tax) << ' ' << round(t3*sales_tax) << endl;
 	//calculates the total after sales_tax and rounding
 	total= round(t1*sales_tax) + round(t2*sales_tax) + round(t3*sales_tax);
