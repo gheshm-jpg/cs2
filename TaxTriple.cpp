1411237576793178c3c39a482773340588cd6ac8
gheshm-jpg <gheshm@bc.edu>
2026-09-16 15:23:36 -0400

    fixed spacing, calculation errors, and added inlines

--- TaxTriple.cpp
+++ TaxTriple.cpp
@@ -14,9 +14,9 @@ int t3;
 
 int main(){
 	sales_tax=1.0625;
-	total= t1*sales_tax + t2*sales_tax + t3*sales_tax;
 	cin >> t1 >> t2 >> t3;		
-	cout << t1*sales_tax << t2*sales_tax << t3*sales_tax;
-	cout << "Total:" << total;	
+	cout << round(t1*sales_tax) << ' ' << round(t2*sales_tax) << ' '<< round(t3*sales_tax) << endl;
+	total= round(t1*sales_tax) + round(t2*sales_tax) + round(t3*sales_tax);
+	cout << "Total: " << total << endl;	
 return 0;
 }
